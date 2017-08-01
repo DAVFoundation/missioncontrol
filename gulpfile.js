b82cbac061eb6bc5a8d195a07f461c05d17d8729
Tal Ater <tal@talater.com>
2017-08-02 00:27:17 +0300

    Gulp now watches thrift IDL files for changes and regenerates code

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const shell = require('gulp-shell');
 
 gulp.task('lint', () => {
   return gulp.src(['**/*.js', '!node_modules/**', '!server/thrift/**'])
@@ -8,8 +9,11 @@ gulp.task('lint', () => {
     .pipe(eslint.failAfterError());
 });
 
+gulp.task('thrift', shell.task('npm run thrift'));
+
 gulp.task('watch', ['default'], () => {
   gulp.watch(['server/**/*.js', '!server/thrift/**'], ['lint']);
+  gulp.watch('resources/idl/*.thrift', ['thrift']);
 });
 
 gulp.task('default', ['lint']);
