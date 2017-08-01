59993fac1582ee5648022f0b70420bdba6f57d57
Tal Ater <tal@talater.com>
2017-08-01 23:25:29 +0300

    Added empty express server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 8443;
+
+
+// Define routes
+app.get('/', function (req, res) {
+    res.send('hello world!');
+});
+
+
+// Start the server
+app.listen(port, function() {
+    console.log(`Mission control server started.`);
+    console.log(`Listening on port ${port}`);
+});
