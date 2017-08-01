4e6a9761ee1af195d5f249bbcb39aa61be5a14f8
Tal Ater <tal@talater.com>
2017-08-02 00:50:50 +0300

    Changed to use gulp-nodemon to start and restart server

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const shell = require('gulp-shell');
+const nodemon = require('gulp-nodemon');
+const path = require('path');
 
 gulp.task('lint', () => {
   return gulp.src(['**/*.js', '!node_modules/**', '!server/thrift/**'])
@@ -11,9 +13,24 @@ gulp.task('lint', () => {
 
 gulp.task('thrift', shell.task('npm run thrift'));
 
-gulp.task('watch', ['default'], () => {
-  gulp.watch(['server/**/*.js', '!server/thrift/**'], ['lint']);
-  gulp.watch('resources/idl/*.thrift', ['thrift']);
-});
+gulp.task('watch', ['default'], () =>
+  nodemon({
+    script: 'server/index.js',
+    ext: 'js thrift',
+    watch: ['server', 'resources/idl'],
+    ignore: [
+      'server/thrift',
+      'node_modules/'
+    ],
+    tasks: (changedFiles) => {
+      let tasks = [];
+      changedFiles.forEach(file => {
+        if (path.extname(file) === '.js' && !tasks.includes('lint')) tasks.push('lint');
+        if (path.extname(file) === '.thrift' && !tasks.includes('thrift')) tasks.push('thrift');
+      });
+      return tasks;
+    }
+  })
+);
 
 gulp.task('default', ['lint']);
