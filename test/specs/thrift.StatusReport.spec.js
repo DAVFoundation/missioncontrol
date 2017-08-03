5ae156d90ab773e7720b0a6e39ac2034b319a47c
Tal Ater <tal@talater.com>
2017-08-03 17:30:40 +0300

    Added basic test for StatusReport is_registered

--- test/specs/thrift.StatusReport.spec.js
+++ test/specs/thrift.StatusReport.spec.js
@@ -1,3 +1,13 @@
-test('true to be true', () => {
-  expect(true).toBe(true);
+const client = require('../../server/client-thrift.js');
+const connection = client.start();
+
+test('is_registered to return false', async () => {
+  expect.assertions(1);
+  await client
+    .getClient()
+    .is_registered()
+    .then(response => {
+      connection.end();
+      expect(response).toBe(false);
+    });
 });
