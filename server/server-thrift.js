ef92701bf22b010e33b329eba46e50318cd35adb
Tal Ater <tal@talater.com>
2017-08-03 16:25:31 +0300

    Added temporary is_registered method to thrift server

--- server/server-thrift.js
+++ server/server-thrift.js
@@ -3,9 +3,15 @@ const StatusReport = require('./thrift/StatusReport.js');
 // const StatusReport_types = require('./thrift/StatusReport_types.js');
 
 const server = thrift.createServer(StatusReport, {
+
+  is_registered: (authenticationToken, vehicleID, result) => {
+    result(null, true);
+  },
+
   report_status: (authenticationToken, vehicleID, state, result) => {
     result(null);
   }
+
 });
 
 module.exports = {
