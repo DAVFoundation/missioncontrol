74e14b2415c70796cbd37394c2e0f0570db955e0
Tal Ater <tal@talater.com>
2017-08-20 21:36:23 +0300

    Use random coordinates in simulated vehicles

--- server/simulation/drone.js
+++ server/simulation/drone.js
@@ -1,11 +1,11 @@
-const { randomDavAddress, randomDroneModel, randomRating } = require('./random');
+const { randomDavAddress, randomDroneModel, randomCoords, randomRating } = require('./random');
 
-const generateRandom = () => {
+const generateRandom = ({coords, distance}) => {
   return {
     'id': randomDavAddress(),
     'model': randomDroneModel(),
     'icon': 'http://lorempixel.com/100/100/abstract/',
-    'coords': {lat: 32.069450, long: 34.772898},
+    'coords': randomCoords({coords, distance}),
     'rating': randomRating(),
     'missions_completed': 36,
     'missions_completed_7_days': 3,
--- server/simulation/vehicles.js
+++ server/simulation/vehicles.js
@@ -1,10 +1,14 @@
 const { generateRandom } = require('./drone');
 
-const getVehicles = () => {
+const getVehicles = ({coords}) => {
+  // Number of vehicles to generate
   const vehicleCount = 4;
+  // Max distance from given coordinates in meters
+  const distance = 1000;
+
   let vehicles = {};
   for (let i = 0; i < vehicleCount; i++) {
-    let vehicle = generateRandom();
+    let vehicle = generateRandom({coords: coords || undefined, distance});
     vehicles[vehicle.id] = vehicle;
   }
   return vehicles;
