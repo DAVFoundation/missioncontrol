d41b56386477b0187ca7757593314a7775f5426f
Tal Ater <tal@talater.com>
2017-08-20 21:45:21 +0300

    Simulated vehicles will be generated around default coordinates if non given

--- server/simulation/vehicles.js
+++ server/simulation/vehicles.js
@@ -3,12 +3,14 @@ const { generateRandom } = require('./drone');
 const getVehicles = ({coords}) => {
   // Number of vehicles to generate
   const vehicleCount = 4;
-  // Max distance from given coordinates in meters
+  // Max distance from given coordinates in meters to generate vehicles in
   const distance = 1000;
+  // Coordinates to generate vehicles around
+  coords = coords || {lat: 47.166167, long: 8.515495};
 
   let vehicles = {};
   for (let i = 0; i < vehicleCount; i++) {
-    let vehicle = generateRandom({coords: coords || undefined, distance});
+    let vehicle = generateRandom({coords, distance});
     vehicles[vehicle.id] = vehicle;
   }
   return vehicles;
