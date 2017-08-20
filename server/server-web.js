3eb9bd75fbe289853125c45e0fe82e5f2ccd42f2
Tal Ater <tal@talater.com>
2017-08-20 17:10:46 +0300

    Added to API /status to return status updates and details about visible drones

--- server/server-web.js
+++ server/server-web.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { getVehicles } = require('./vehicles');
 
 const app = express();
 const port = process.env.WEB_SERVER_PORT || 8888;
@@ -9,6 +10,14 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
 
+app.get('/status', function (req, res) {
+  res.send({
+    vehicles: getVehicles({
+      userId: req.query.id,
+      coords: { lat: req.query.lat, long: req.query.long }
+    })
+  });
+});
 
 
 module.exports = {
