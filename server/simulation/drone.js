1e43540041ef2d250df30a6760861f5266cfa53d
Tal Ater <tal@talater.com>
2017-08-20 17:16:19 +0300

    Added a random icon for random drones

--- server/simulation/drone.js
+++ server/simulation/drone.js
@@ -4,6 +4,7 @@ const generateRandom = () => {
   return {
     'id': randomDavAddress(),
     'model': randomDroneModel(),
+    'icon': 'http://lorempixel.com/100/100/abstract/',
     'coords': {lat: 32.069450, long: 34.772898},
     'rating': 4.9,
     'missions_completed': 36,
