fbb56e9fa0afb62e2f7438871a461112b627f975
Tal Ater <tal@talater.com>
2017-08-22 09:59:34 +0300

    Status api now returns vehicles in an array instead of object

--- server/simulation/vehicles.js
+++ server/simulation/vehicles.js
@@ -8,10 +8,9 @@ const getVehicles = ({coords}) => {
   // Coordinates to generate vehicles around
   coords = coords || {lat: 47.166167, long: 8.515495};
 
-  let vehicles = {};
+  let vehicles = [];
   for (let i = 0; i < vehicleCount; i++) {
-    let vehicle = generateRandom({coords, distance});
-    vehicles[vehicle.id] = vehicle;
+    vehicles.push(generateRandom({coords, distance}));
   }
   return vehicles;
 };
