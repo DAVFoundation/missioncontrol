b0da45344afbd16e7b47c1d5650db36ad54d58a3
Ankit Sinha <asinha@thecirqus.com>
2017-08-23 16:24:36 +0530

    Added tests to check randomDavAddress() #18

--- test/specs/simulation.random.spec.js
+++ test/specs/simulation.random.spec.js
@@ -1,4 +1,4 @@
-const { randomDroneModel } = require('../../server/simulation/random');
+const { randomDroneModel, randomDavAddress } = require('../../server/simulation/random');
 
 describe('randomDroneModel()', () => {
 
@@ -12,6 +12,22 @@ describe('randomDroneModel()', () => {
     expect(
       randomDroneModel().split(' ').length
     ).toBeGreaterThanOrEqual(2);
+  });
+
+});
+
+describe('randomDavAddress()', () => {
+
+  test('returns a string', () => {
+    expect(
+      typeof randomDavAddress()
+    ).toBe('string');
+  });
+
+  test('returns a string that is 42 characters long', () => {
+    expect(
+      randomDavAddress().length
+    ).toEqual(42);
   });
 
 });
