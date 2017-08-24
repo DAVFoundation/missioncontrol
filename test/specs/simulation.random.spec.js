4df96efe88d4a1e55ca98566b9562f5ce81efe6e
Gavrilov Artem <gavrilov-a@speechpro.com>
2017-08-24 10:51:44 +0300

    Add tests for issue #31 #27 #28 (testing randomMissionsCompleted function)

--- test/specs/simulation.random.spec.js
+++ test/specs/simulation.random.spec.js
@@ -1,4 +1,4 @@
-const { randomDroneModel, randomDavAddress } = require('../../server/simulation/random');
+const { randomDroneModel, randomDavAddress, randomMissionsCompleted } = require('../../server/simulation/random');
 
 describe('randomDroneModel()', () => {
 
@@ -28,6 +28,41 @@ describe('randomDavAddress()', () => {
     expect(
       randomDavAddress().length
     ).toEqual(42);
+  });
+
+});
+
+describe('randomMissionsCompleted()', () => {
+
+  test('returns a object', () => {
+
+    expect(typeof randomMissionsCompleted()).toBe('object');
+
+  });
+
+  test('returns a object with two keys', () => {
+
+    const mock = randomMissionsCompleted();
+
+    expect(mock).toHaveProperty('missionsCompleted', expect.any(Number));
+
+    expect(mock).toHaveProperty('missionsCompleted7Days', expect.any(Number));
+
+  });
+
+  test('missionsCompleted7Days less than missionsCompleted and greater than 0', () => {
+
+    for(i = 0; i < 10; i++) {
+
+      const mock = randomMissionsCompleted();
+
+      expect(mock.missionsCompleted7Days).toBeGreaterThan(0);
+
+      expect(mock.missionsCompleted7Days)
+        .toBeLessThanOrEqual(mock.missionsCompleted);
+
+    }
+
   });
 
 });
