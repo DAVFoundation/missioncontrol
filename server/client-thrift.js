8fe6f05c6edf85ec79fea8cc96acf823e9a77ae9
Tal Ater <tal@talater.com>
2017-08-26 16:19:44 +0300

    Changed the way client-thrift methods are exported

--- server/client-thrift.js
+++ server/client-thrift.js
@@ -16,24 +16,27 @@ const mp = new thrift.Multiplexer();
 let connection;
 let clients = {};
 
+const start = ({port = 9090} = {}) => {
+  connection = thrift.createConnection(host, port, {
+    transport: transport,
+    protocol: protocol,
+  });
+
+  connection.on('error', function(err) {
+    console.log('Connection error');
+    console.log(err);
+  });
+  return connection;
+};
+
+const getClient = (service) => {
+  if (!clients[service]) {
+    clients[service] = mp.createClient(service, services[service], connection);
+  }
+  return clients[service];
+};
+
 module.exports = {
-  start: ({port = 9090} = {}) => {
-    connection = thrift.createConnection(host, port, {
-      transport: transport,
-      protocol: protocol,
-    });
-
-    connection.on('error', function(err) {
-      console.log('Connection error');
-      console.log(err);
-    });
-    return connection;
-  },
-
-  getClient: (service) => {
-    if (!clients[service]) {
-      clients[service] = mp.createClient(service, services[service], connection);
-    }
-    return clients[service];
-  },
+  start,
+  getClient,
 };
