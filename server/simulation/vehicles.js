e8c3b91010445d18c72e46312f460df299626ec8
Tal Ater <tal@talater.com>
2017-09-03 19:38:46 +0300

    Added method to return a random bid (still no randomness)

--- server/simulation/vehicles.js
+++ server/simulation/vehicles.js
@@ -8,6 +8,15 @@ const generateRandomVehicles = (vehicleCount = 4, coords, radius = 2000) => {
   return vehicles;
 };
 
+const randomBid = () => {
+  return {
+    bid: 0.8,
+    pickup: Date.now()+(1000*60*2),
+    dropoff: Date.now()+(1000*60*10),
+  };
+};
+
 module.exports = {
-  generateRandomVehicles
+  generateRandomVehicles,
+  randomBid
 };
