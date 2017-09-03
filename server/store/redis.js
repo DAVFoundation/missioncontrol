216937268db8726db98a05d5f0b4648732af382b
Tal Ater <tal@talater.com>
2017-09-04 00:40:15 +0300

    Load redis settings from environment variables

--- server/store/redis.js
+++ server/store/redis.js
@@ -2,11 +2,13 @@ const { hasStore } = require('../lib/environment');
 
 if (hasStore) {
 
+  const host = process.env.REDIS_HOST || 'redis';
+  const port = process.env.REDIS_PORT || 6379;
   const bluebird = require('bluebird');
   const redis = require('redis');
   bluebird.promisifyAll(redis.RedisClient.prototype);
 
-  const client = redis.createClient({ host: 'redis', port: 6379 });
+  const client = redis.createClient({ host: host, port: port });
 
   module.exports = client;
 
