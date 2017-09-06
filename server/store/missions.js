7889ad4f6dc5b216d5d14a6c9d20b11dd4a6205b
Tal Ater <tal@talater.com>
2017-09-06 13:06:02 +0300

    Added store/missions.js with method to store mission details in Redis

---
+++ server/store/missions.js
@@ -0,0 +1,38 @@
+const redis = require('./redis');
+const { getBid } = require('./bids');
+const { getRequest } = require('./requests');
+
+const createMission = async ({ user_id, bid_id }) => {
+  // get bid details
+  const bid = await getBid(bid_id);
+  const { vehicle_id, price, pickup_time, dropoff_time, request_id } = bid;
+
+  // get request details
+  const request = await getRequest(request_id);
+  const { pickup_lat, pickup_long, dropoff_lat, dropoff_long, requested_pickup_time, size, weight } = request;
+
+  // get new unique id for mission
+  const missionId = await redis.incrAsync('next_mission_id');
+
+  // create a new mission entry in Redis
+  redis.hmsetAsync(`missions:${missionId}`,
+    'user_id', user_id,
+    'vehicle_id', vehicle_id,
+    'price', price,
+    'pickup_time', pickup_time,
+    'dropoff_time', dropoff_time,
+    'request_id', request_id,
+    'pickup_lat', pickup_lat,
+    'pickup_long', pickup_long,
+    'dropoff_lat', dropoff_lat,
+    'dropoff_long', dropoff_long,
+    'requested_pickup_time', requested_pickup_time,
+    'size', size,
+    'weight', weight,
+  );
+  return missionId;
+};
+
+module.exports = {
+  createMission
+};
