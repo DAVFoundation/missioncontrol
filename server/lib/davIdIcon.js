d384e6f0439fc96541d5470e46efe36a3f77c9a3
cg-cnu <sreenivas9alapati@gmail.com>
2017-10-01 11:40:14 +0530

    fixed indentation and missing ;

--- server/lib/davIdIcon.js
+++ server/lib/davIdIcon.js
@@ -4,11 +4,11 @@
  * @param {String} davId. A string of length 42 beginning with 0x followed by 40 hexadecimal characters
  * @returns {string}  An URL to the icon
  */
- const getDavIdIconUrl = davId => {
+const getDavIdIconUrl = davId => {
   if (!davId){
-    return new Error('No valid davId provided')
+    return new Error('No valid davId provided');
   }
-  return `https://lorempixel.com/100/100/abstract/?${davId}`
+  return `https://lorempixel.com/100/100/abstract/?${davId}`;
 };
 
 module.exports = {
