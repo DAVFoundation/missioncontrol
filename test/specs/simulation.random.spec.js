00f5cd99ae755fc8b157fbe4da2a2efde977be8b
Griffin Obeid <griffobeid@gmail.com>
2018-01-06 13:35:34 -0500

    Added a test for randomCoords(). Tests if the distance between the given coordinates and the returned coordinates is within the given radius.

--- test/specs/simulation.random.spec.js
+++ test/specs/simulation.random.spec.js
@@ -2,6 +2,7 @@ const {
   randomDroneModel,
   randomDavAddress,
   randomMissionsCompleted,
+  randomCoords,
 } = require('../../server/simulation/random');
 
 describe('randomDroneModel()', () => {
@@ -73,5 +74,35 @@ describe('randomMissionsCompleted()', () => {
     expect(
       randomMissionsCompleted().missionsCompleted
     ).toBeGreaterThanOrEqual(4);
+  });
+});
+
+describe('randomCoords()', () => {
+  const sampleArguments = { coords: { lat: 1, long: 1 }, radius: 1000 };
+  const coords = {
+    lat1: sampleArguments.coords.lat,
+    long1: sampleArguments.coords.lat,
+    lat2: randomCoords(sampleArguments).lat,
+    long2: randomCoords(sampleArguments).long
+  };
+  /**
+   * Haversine Formula: returns the distance from one point to another
+   * @returns {Number} a number containing distance in meters
+   */
+  const getDistance = ({ lat1, long1, lat2, long2 }) => {
+    const R = 6371; // radius of the earth in km
+    const dLat = degToRad(lat2 - lat1);
+    const dLong = degToRad(long2 - long1);
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = (R * c) * 1000;
+    return d;
+  };
+  const degToRad = (deg) => { return deg * (Math.PI / 180); };
+
+  test('returns a coordinate that is no further than given coordinates by given radius', () => {
+    expect(
+      getDistance(coords)
+    ).toBeLessThanOrEqual(sampleArguments.radius);
   });
 });
