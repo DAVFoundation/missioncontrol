3b0970cfc7ca708119145e5b0f40a9c57015411f
Timi Ajiboye <timi@helloworld.ng>
2018-01-21 03:03:24 +0100

    feat: completed vehicle motion while in mission

--- server/simulation/vehicles.js
+++ server/simulation/vehicles.js
@@ -18,12 +18,34 @@ const randomBid = (origin, pickup, dropoff) => {
   const price = parseFloat(distanceOriginToPickup + distancePickupToDelivery);
   return {
     price: price,
-    time_to_pickup: distanceOriginToPickup * 3,
-    time_to_dropoff: distancePickupToDelivery * 3 * Math.random(),
+    time_to_pickup: distanceOriginToPickup * 3 * 60000, // multiplied by 60000 to keep everything in milliseconds
+    time_to_dropoff: distancePickupToDelivery * 3 * 60000 * Math.random(),
   };
+};
+
+const calculateNextCoordinate = async (vehicle, mission, leg, positionLastUpdatedAt, previousPosition) => {
+  const legStartTime = leg == 'pickup' ? mission.user_signed_at : mission.travelling_dropoff_at
+  const legCompletionTime = parseFloat(legStartTime) + parseFloat(mission['time_to_' + leg])
+  const destinationLong = mission[leg + '_long'];
+  const destinationLat = mission[leg + '_lat']
+
+  const timeLeftAtPreviousPosition = legCompletionTime - positionLastUpdatedAt;
+
+  const previouslyUncoveredDistanceLong = destinationLong - previousPosition.long;
+  const previouslyUncoveredDistanceLat = destinationLat - previousPosition.lat;
+
+  const speedLong = previouslyUncoveredDistanceLong / timeLeftAtPreviousPosition;
+  const speedLat = previouslyUncoveredDistanceLat / timeLeftAtPreviousPosition;
+
+  const timeLeftAtNewPosition = legCompletionTime - Date.now();
+  const long = (destinationLong - (timeLeftAtNewPosition * speedLong)).toFixed(6);
+  const lat = (destinationLat - (timeLeftAtNewPosition * speedLat)).toFixed(6);
+
+  return {long, lat};
 };
 
 module.exports = {
   generateRandomVehicles,
+  calculateNextCoordinate,
   randomBid,
 };
