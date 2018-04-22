2d0cf2a0d9b49a78678de9e7ee0ed4dd8508396e
David Mauas <davidmauas@Davids-MacBook-Pro.local>
2018-04-22 21:14:39 +0300

    changes made according to shahar's requests

--- server/lib/email.js
+++ server/lib/email.js
@@ -1,13 +1,6 @@
 const API_KEY = 'key-099572ee6cd2260a358eeb65b3eeeef8';
 const DOMAIN = 'mg.dav.network';
 const mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
- 
-// const MAILGUN_DATA = {
-//   from: 'Excited User <noreply@dav.network>',
-//   to: 'dmauas@gmail.com',
-//   subject: 'Hello',
-//   text: 'Testing some Mailgun awesomeness!'
-// };
 
 const mail = async (from,to,title,body) => {
   let MAILGUN_DATA = {
@@ -17,7 +10,7 @@ const mail = async (from,to,title,body) => {
     text: body
   };
   //code to send mail here
-  mailgun.messages().send(MAILGUN_DATA, function (error, responseBody) {
+  await mailgun.messages().send(MAILGUN_DATA, function (error, responseBody) {
     console.log(responseBody);
   });
   return;
--- test/specs/gradd.spec.js
+++ test/specs/gradd.spec.js
@@ -10,23 +10,3 @@ describe('updateVehicleStatus', ()=>{
     ).toBe(true);
   });
 });
-
-// const { getDavIdIconUrl } = require('../../server/lib/davIdIcon.js');
-// const { checkIfAbsoluteURL } = require('../helpers/index.js');
-// const dummyDavId = '0xdfddfdfdfdfdfdfdfdfdfdfdfdfddfdfdfdfdfdd';
-/*
-describe('getDavIdIconUrl()', () => {
-  test('returns an absolute URL', () => {
-    expect(
-      checkIfAbsoluteURL(getDavIdIconUrl(dummyDavId))
-    ).toBe(true);
-  });
-
-  test('returns a string containing the given davID', () => {
-    expect(
-      getDavIdIconUrl(dummyDavId).includes(dummyDavId)
-    ).toBe(true);
-  });  
-  
-});
-*/
