c1ae25438908e1d707e5188a7665cb238dbd07dd
srfrnk <srfrnk@gmail.com>
2018-04-25 14:55:44 +0300

    fix: merge fixes

--- server/controllers/NeedController.js
+++ server/controllers/NeedController.js
@@ -1,18 +1,18 @@
 const { createNeed, getNeed, deleteNeed } = require('../store/needs');
 const { deleteBidsForNeed } = require('../store/bids');
 const { getCaptainsForNeedType, addNeedToCaptain, getNeeds } = require('../store/captains');
-const createConstraints = require('./constraints/need/create');
-const validate = require('../lib/validate');
+// const createConstraints = require('./constraints/need/create');
+// const validate = require('../lib/validate');
 
 const create = async (req, res) => {
   const params = req.body;
-  const validationErrors = validate(params, createConstraints);
+  const validationErrors = null;//validate(params, createConstraints);
   if (validationErrors) {
     res.status(422).json(validationErrors);
   } else {
     try {
-      const allowedParamsKeys = Object.keys(createConstraints);
-      Object.keys(params).forEach(key => { if (!allowedParamsKeys.includes(key)) delete params[key]; });
+      // const allowedParamsKeys = Object.keys(createConstraints);
+      // Object.keys(params).forEach(key => { if (!allowedParamsKeys.includes(key)) delete params[key]; });
       params.user_id = req.query.user_id;
 
       let needId = await createNeed(params);
@@ -25,7 +25,7 @@ const create = async (req, res) => {
           latitude: params.dropoff_latitude
         }
       };
-      
+
       let captains = await getCaptainsForNeedType(params.need_type, terminals);
       await Promise.all(captains.map(async captain => await addNeedToCaptain(captain.id, needId, params.ttl)));
       res.json({ needId });
--- server/store/needs.js
+++ server/store/needs.js
@@ -1,6 +1,5 @@
 const redis = require('./redis');
 const config = require('../config');
-const { getVehiclesInRange } = require('./vehicles');
 
 const getNeed = async needId => {
   // Set TTL for need
@@ -13,9 +12,6 @@ const createNeed = async needDetails => {
   const needId = await redis.incrAsync('next_need_id');
   const key_value_array = [].concat(...Object.entries(needDetails));
   redis.hmsetAsync(`needs:${needId}`, ...key_value_array);
-
-  // See if there are any vehicles around the pickup position, if not a few vehicles will be generated there
-  getVehiclesInRange({ lat: parseFloat(needDetails.pickup_latitude), long: parseFloat(needDetails.pickup_longitude) }, 7000);
 
   // Set TTL for need
   setNeedTTL(needId);
