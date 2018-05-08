0177f51d5e05a1bd5b383f8b487d28dbb0b6d24f
dmauas <dmauas@gmail.com>
2018-05-08 08:36:02 -0400

    using mailcomposer for html email

--- server/lib/email.js
+++ server/lib/email.js
@@ -1,19 +1,28 @@
 const API_KEY = 'key-099572ee6cd2260a358eeb65b3eeeef8';
 const DOMAIN = 'mg.dav.network';
 const mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+var mailcomposer = require('mailcomposer');
 
 const mail = async (from,to,title,body) => {
-  let MAILGUN_DATA = {
+  let mail = mailcomposer({
     from: from,
     to: to,
     subject: title,
-    text: body
-  };
-  //code to send mail here
-  await mailgun.messages().send(MAILGUN_DATA, function (error, responseBody) {
-    console.log(responseBody);
+    body: body,
+    html: body
   });
-  return;
+  mail.build(function(mailBuildError, message) {
+    var dataToSend = {
+      to: to,
+      message: message.toString('ascii')
+    };
+    mailgun.messages().sendMime(dataToSend, function (sendError, body) {
+      if (sendError) {
+        console.log(sendError);
+        return;
+      }
+    });
+  });
 };
 
 module.exports = {
