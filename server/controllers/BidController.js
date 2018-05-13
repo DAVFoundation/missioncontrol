bd5f8ec0da39207e6bb7cc4f5277a6c8752a7e53
Anatoly T <antt001@gmail.com>
2018-05-13 15:51:14 +0300

    fix: remove email sending from chhoseBid(TODO: implement in captain)

--- server/controllers/BidController.js
+++ server/controllers/BidController.js
@@ -47,7 +47,6 @@ const chooseBid = async (req, res) => {
   if (mission) {
     await addBidToCaptain(mission.captain_id, bidId);
     await updateCaptainStatus(mission.captain_id, 'contract_received');
-    await emailGraddStatusPayloadRequest(mission.mission_id);
     res.json({ mission });
   } else {
     res.status(500).send('Something broke!');
