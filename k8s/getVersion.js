e4b38fcf8d8efde8917f1062524ae8848f52277b
srfrnk <srfrnk@gmail.com>
2018-09-27 14:08:31 +0300

    fix: DD-321 fixed

---
+++ k8s/getVersion.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const drc = require('docker-registry-client');
+const client = drc.createClientV2({
+  name: 'davnetwork/dav-api'
+});
+client.listTags(function (err, res) {
+  const tag = res.tags.filter(tag => /^\d{6}\-\d{4}$/.test(tag)).sort().reverse()[0];
+  console.log(tag);
+  client.close();
+});
