d4a64e267aadc660e2bc2a2e606cab31413ca750
Vladimir Neverov (meddle) <sanguini@gmail.com>
2018-12-12 00:58:04 +0200

    Add 'load' test for ride_hailing provider

--- src/provider/RideHailingProvider.test.ts
+++ src/provider/RideHailingProvider.test.ts
@@ -18,9 +18,27 @@ describe('Ride Hailing Provider', () => {
       },
     },
   };
+  const need: INeed = {
+    topicId: '222',
+    protocol: 'ride_hailing',
+    data: {
+      pickupLocation: {
+        longitude: 1,
+        latitude: 1,
+      },
+      destinationLocation: {
+        longitude: 1,
+        latitude: 1,
+      },
+    },
+  };
 
   it('should save provider', async () => {
       const rideHailingProvider = new RideHailingProvider();
       expect(await rideHailingProvider.save(provider)).toBe(true);
+  });
+  it('should load provider', async () => {
+    const rideHailingProvider = new RideHailingProvider();
+    expect(await rideHailingProvider.query(need)).toEqual([provider]);
   });
 });
