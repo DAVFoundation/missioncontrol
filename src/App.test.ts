cc48741e987c51ffcbc58eddc712c1e2bf224874
Vladimir Neverov (meddle) <sanguini@gmail.com>
2018-12-24 00:59:53 +0300

    Add test for connected Kafka status in the healthy test suite

--- src/App.test.ts
+++ src/App.test.ts
@@ -86,6 +86,11 @@ describe('App', () => {
       const res = await chai.request(app).get('/health');
       expect(res.body.message.app.connected).to.eql(true);
     });
+
+    it('should return connected status for Kafka', async () => {
+      const res = await chai.request(app).get('/health');
+      expect(res.body.message.kafka.connected).to.eql(true);
+    });
   });
   describe('status (failure)', () => {
     let app: Application;
