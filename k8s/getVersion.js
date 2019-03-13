c8470b1d01a617326a45595fcf9a4fa170f3e3d0
Tal Ater <tal@talater.com>
2019-03-13 09:25:15 +0200

    minor: formatting fix

--- k8s/getVersion.js
+++ k8s/getVersion.js
@@ -2,10 +2,13 @@
 
 const drc = require('docker-registry-client');
 const client = drc.createClientV2({
-  name: 'davnetwork/dav-api'
+  name: 'davnetwork/dav-api',
 });
-client.listTags(function (err, res) {
-  const tag = res.tags.filter(tag => /^\d{6}\-\d{4}$/.test(tag)).sort().reverse()[0];
+client.listTags(function(err, res) {
+  const tag = res.tags
+    .filter(tag => /^\d{6}\-\d{4}$/.test(tag))
+    .sort()
+    .reverse()[0];
   console.log(tag);
   client.close();
 });
