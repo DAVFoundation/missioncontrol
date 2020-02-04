f6e2c2b649dbdd2c8539331760d9c4d740ff7bff
Jatin <b4jatin@gmail.com>
2020-02-04 13:49:01 +1100

    tesing github repo

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,34 @@ gulp.task('spellcheck', function(done) {
     .pipe(spellcheck.report({}));
 });
 
+gulp.task('tslint', done => {
+  return gulp
+    .src('src/**/*.ts')
+    .on('error', function(err) {
+      done(err);
+    })
+    .pipe(
+      tslint({
+        formatter: 'prose',
+      }),
+    )
+    .pipe(tslint.report());
+});
+
+gulp.task('spellcheck', function(done) {
+  return gulp
+    .src('src/**/*.ts')
+    .on('error', function(err) {
+      done(err);
+    })
+    .pipe(
+      spellcheck({
+        dictionary: require('./speller-dictionary.js'),
+      }),
+    )
+    .pipe(spellcheck.report({}));
+});
+
 gulp.task('tsc', function(done) {
   var tsProject = ts.createProject('tsconfig.json');
   return tsProject
